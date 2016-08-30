3922b982c291bd2fa4ab5848560af00b61246b9d
Colin Aulds <caulds989@gmail.com>
2016-08-30 13:26:38 -0700

    changed lab database to dedicate db for game swap

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://caulds989:Seoul989@ds031975.mlab.com:31975/mongo-shopping-list-aulds' :
+                            'mongodb://caulds989:Seoul989@ds019976.mlab.com:19976/gameswap' :
                             'mongodb://localhost/gameswap');
 exports.PORT = process.env.PORT || 8080;
